1c70c05e915cce4632f75919130e2c4c20382731
ctsjava <ctsgit@gmail.com>
2020-06-10 13:13:53 +0530

    ts

---
+++ JS/es6/1-arrow-operator.js
@@ -0,0 +1,37 @@
+// function expressions
+
+
+//let getPrice=function(){
+//    return 1500;
+//}
+
+// or 
+
+//let getPrice=()=>{
+//   return 700;
+//}
+
+//----------------------------------------------------------------
+
+//let getPrice = (count) => {
+//   return count * 1200;
+//}
+
+// or
+
+//let getPrice = count => {
+//   return count * 100;
+//}
+
+//let getPrice =(count,tax)=>{
+//  return count*(100+tax)
+//}
+
+// or
+
+//let getPrice = (count, tax) =>  count * (100 + tax);
+
+
+
+
+
---
+++ JS/es6/2-spread-operator.js
@@ -0,0 +1,123 @@
+
+let arr1 = [1, 2, 3];
+let arr2 = [7, 8, 9];
+
+let arr3 = [...arr1, 4, 5, 6, ...arr2];
+
+let arr = [40, 30, 20, 70, 10]
+
+let maxVal = Math.max(...arr);
+
+let o1 = {
+    x: 10,
+    y: 20
+}
+
+let o2 = {
+    z: 30,
+    x: 1
+}
+
+let o3 = { ...o1, ...o2 }
+
+let o4 = Object.assign({ a1: 100, b1: 200 }, o2, o3);
+
+let todos = [
+    { body: 'play', completed: true },
+    { body: 'sleep', completed: false },
+    { body: 'study', completed: true },
+    { body: 'eat', completed: false },
+    { body: 'drive', completed: true }
+]
+
+// task1
+// copy all todos into new array whose statis is true
+
+// how to iterate
+//for(let index in todos){
+// console.log(todos[index]); 
+//}
+
+//for(let todo of todos){
+// console.log(todo);   
+//}
+
+let completed = [];
+//todos.map((todo, index) => {
+//console.log(index  + ' :  '+ JSON.stringify(todo));
+//console.log(todo);
+// completed = Object.assign((todo.completed ? Object.assign(todo) : {...completed}));
+//})
+
+class Todo {
+    constructor(title, date) {
+        Todo.nextId++;
+        this.id = Todo.nextId;
+        this.title = title;
+        this.completed = false;
+        this.date = date;
+    }
+
+}
+Todo.nextId = 0;
+class TodoService {
+    constructor() {
+        this.todos = [];
+    }
+
+
+    addTodo(title) {
+        let newTodo = new Todo(title, new Date().toLocaleString());
+        this.todos = this.todos.concat(newTodo);
+    }
+
+    editTodo(id, newTitle) {
+        this.todos = this.todos.map(todo => todo.id == id ? Object.assign({}, todo, { title: newTitle }) : todo);
+    }
+
+    completedTodo(id) {
+        this.todos = this.todos.map(todo => todo.id == id ? Object.assign({}, todo, { completed: !todo.completed }) : todo)
+    }
+    completedAll() {
+        let isAllCompleted = this.todos.every(todo => todo.completed);
+        this.todos = this.todos.map(todo => Object.assign({}, todo, { completed: !isAllCompleted }))
+    }
+    deleteTodo(id) {
+        this.todos = this.todos.filter(todo => todo.id !== id);
+    }
+
+    viewTodos() {
+        this.todos.forEach(todo => console.log(todo))
+        this.todos.forEach(todo => {
+            var data = document.getElementById("todos");
+            var node = document.createElement("input");
+            var chkBox = document.createElement('input');
+            chkBox.type = 'checkbox';
+            var newLine = document.createElement('br');
+            node.type = "text";
+            node.value = todo.title;
+            var textNode = document.createTextNode(todo.title);
+            node.appendChild(textNode);
+            document.getElementById("todos").appendChild(chkBox);
+            document.getElementById("todos").appendChild(node);
+            document.getElementById("todos").appendChild(newLine);
+
+
+
+        })
+    }
+}
+
+
+const service = new TodoService();
+
+service.addTodo('Learn Javascript ')
+service.addTodo('Learn Java')
+service.addTodo('Learn Life ')
+
+
+
+
+
+
+
